cdae5b8a5d9a30b5cee65a778163847ae7d7c80c
Steph Noutsa <stephanenoutsak@gmail.com>
2022-01-07 08:14:53 +0100

    Updates captcha prediction URL

--- src/services/v0/captcha/captcha.service.js
+++ src/services/v0/captcha/captcha.service.js
@@ -3,7 +3,7 @@ import fs from 'fs'
 import axios from 'axios'
 import FormData from 'form-data'
 
-const PREDICT_URL = 'http://localhost:5000/predict'
+const PREDICT_URL = 'http://137.184.12.159/predict'
 
 export const getDigitsFromCaptchaImage = async img => {
     const formData = new FormData()
